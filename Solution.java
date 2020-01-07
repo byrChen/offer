1e97068ac71f3146676c75667268fd81b3bcc795
byrChen <byrChenXF@gmail.com>
2020-01-07 16:19:23 +0800

    31

--- Solution.java
+++ Solution.java
@@ -1,40 +1,53 @@
+import java.util.*;
 public class Solution {
+//    public static boolean IsPopOrder(int [] pushA,int [] popA) {
+//        int length=popA.length;
+//        if(length==0)
+//            return true;
+//        if(length==1)
+//            return popA[0]==pushA[0];
+//        int i;
+//        for (i = 0; i < popA.length; i++) {
+//            if(popA[i]==pushA[0])
+//                break;
+//        }
+//        return IsPopOrder(Arrays.copyOfRange(pushA, 1, i+1),Arrays.copyOfRange(popA, 0, i)) &&
+//                IsPopOrder(Arrays.copyOfRange(pushA, i+1, length),Arrays.copyOfRange(popA, i+1,length));
+//    }
 
-    private TreeNode root;
-
-
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) {
-            val = x;
+    public static boolean IsPopOrder(int[] pushA,int[] popA) {
+        if (pushA == null || popA == null || pushA.length != popA.length) return false;
+        if (pushA.length == 0) return true;
+        if (pushA.length == 1) return pushA[0] == popA[0];
+        int first = popA[0], index = -1;
+        for (int i : pushA) {
+            index += 1;
+            if (i == first) break;
         }
+        if (index == -1) return false;
+        int[] left = Arrays.copyOfRange(pushA, 0, index);
+        int[] right = Arrays.copyOfRange(pushA, index+1, pushA.length);
+        int leftEnd = index, rightStart = index + 1;
+        if (index != 0 && popA[1] != left[index-1]) {
+            leftEnd = popA.length - 1;
+            rightStart = 1;
+        }
+        int[] rightInpopA = Arrays.copyOfRange(popA, rightStart, rightStart+right.length);
+        return IsPopOrder(right, rightInpopA) && reverse(left, popA, leftEnd);
     }
 
-    public TreeNode reConstructBinaryTree(int[] pre,int[] in) {
-        if (pre == null || in == null || pre.length != in.length) return null;
-        if (pre.length == in.length && pre.length <= 1) return new TreeNode(pre[0]);
-        TreeNode t = new TreeNode(pre[0]);
-        int index = 0;
-        while (in[index] != pre[0]) {
-            index += 1;
+    private static boolean reverse(int[] a, int[] b, int index) {
+        if (index != 0) {
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != b[index-i]) return false;
+            }
         }
-        int lLen = index, rLen = pre.length - index - 1;
-        int[] lpre = new int[lLen], lin = new int[lLen];
-        int[] rpre = new int[rLen], rin = new int[rLen];
-        System.arraycopy(pre, 1, lpre, 0, lLen);
-        System.arraycopy(pre, index+1, rpre, 0, rLen);
-        System.arraycopy(in, 0, lin, 0, lLen);
-        System.arraycopy(in, index+1, rin, 0, rLen);
-        t.left = reConstructBinaryTree(lpre, lin);
-        t.right = reConstructBinaryTree(rpre, rin);
-        return t;
+        return true;
     }
 
     public static void main(String[] args) {
-        int[] pre = {1,2,3,4,5,6,7}, in = {3,2,4,1,6,5,7};
-        Solution s = new Solution();
-        System.out.println(s.reConstructBinaryTree(pre, in).val);
+        int[] a = {1,2};
+        int[] b = {0,2};
+        System.out.println(IsPopOrder(a, b));
     }
 }
